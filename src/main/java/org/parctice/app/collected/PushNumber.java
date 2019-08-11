1bcd799730a3a721dd11d9806d47508328ab0783
Alex <alex.d.bondarev@gmail.com>
2019-08-11 14:46:46 +0300

    refactor PushNumber

--- src/main/java/org/parctice/app/collected/PushNumber.java
+++ src/main/java/org/parctice/app/collected/PushNumber.java
@@ -1,30 +1,39 @@
 package org.parctice.app.collected;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 /**
  * Push all given numbers to the end of array
  */
 
 public class PushNumber {
-    public static void main(String[] args){
-        int testArray[] = {0, 1, 2, 3, 0, 4, 5, 0, 0, 0, 6, 0, 0};
-        int numToPush = 0;
-        System.out.println(Arrays.toString(pushArrayNumberToEnd(testArray, numToPush)));
+    public static void main(String[] args) {
+        int testArray[] = {0, 1, 2, 3, 0, 4, 5, 6, 0, 9};
+        int num1ToPush = 7;
+        int num2ToPush = 6;
+        int num3ToPush = 5;
+
+        System.out.println("First try was: \n" +
+                Arrays.toString(pushArrayNumberToEnd(testArray,
+                        num1ToPush, num2ToPush, num3ToPush)));
+
+        System.out.println("But this looks better: \n" +
+                Arrays.toString(pushArrayNumberToEndViaStream(testArray,
+                        num1ToPush, num2ToPush, num3ToPush)));
     }
 
-    public static int[] pushArrayNumberToEnd(int[] array, int number){
-        int arrayLength = array.length;
-        int counter = 0;
-        for(int i = 0 ; i < arrayLength; i++){
-            array[counter] = array[i];
-            if(array[i] != number)
-                counter++;
-        }
+    public static int[] pushArrayNumberToEnd(int[] array, int... numbers) {
 
-        while (counter < arrayLength)
-            array[counter++] = 0;
+        int[] newArray = new int[array.length + numbers.length];
 
-        return array;
+        System.arraycopy(array, 0, newArray, 0, array.length);
+        System.arraycopy(numbers, 0, newArray, array.length, numbers.length);
+
+        return newArray;
+    }
+
+    public static int[] pushArrayNumberToEndViaStream(int[] array, int... numbers) {
+        return IntStream.concat(Arrays.stream(array), Arrays.stream(numbers)).toArray();
     }
 }
