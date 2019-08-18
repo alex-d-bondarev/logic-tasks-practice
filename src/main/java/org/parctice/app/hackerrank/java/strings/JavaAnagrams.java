07325f88119772d6554f38d5d21123350b0aa9d4
Alex <alex.d.bondarev@gmail.com>
2019-08-18 16:51:19 +0300

    add JavaAnagrams without extra imports

---
+++ src/main/java/org/parctice/app/hackerrank/java/strings/JavaAnagrams.java
@@ -0,0 +1,45 @@
+package org.parctice.app.hackerrank.java.strings;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+// This task does not allow additional imports
+
+public class JavaAnagrams {
+
+    static boolean isAnagram(String a, String b) {
+        char[] aArray = a.toLowerCase().toCharArray();
+        char[] bArray = b.toLowerCase().toCharArray();
+
+        a = new String(sortArray(aArray));
+        b = new String(sortArray(bArray));
+
+        return a.equals(b);
+    }
+
+    static char[] sortArray(char[] array){
+        char tempChar;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0 + i; j < array.length; j++) {
+                if (array[i] > array[j]){
+                    tempChar = array[i];
+                    array[i] = array[j];
+                    array[j] = tempChar;
+                }
+            }
+        }
+
+        return array;
+    }
+
+    @Test
+    public void thisIsAnagram(){
+        Assert.assertTrue(isAnagram("anagram", "margana"));
+    }
+
+    @Test
+    public void thisIsNotAnagram(){
+        Assert.assertFalse(isAnagram("anagramm", "marganaa"));
+    }
+}
