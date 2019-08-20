60b4ee855fa4eee2c5e99d675b9086a3962cbd1e
Alex <alex.d.bondarev@gmail.com>
2019-08-20 22:18:06 +0300

    add Java1DArray

---
+++ src/main/java/org/parctice/app/hackerrank/java/data_structures/Java1DArray.java
@@ -0,0 +1,27 @@
+package org.parctice.app.hackerrank.java.data_structures;
+
+import java.util.Scanner;
+
+public class Java1DArray {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        // Add code here
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = scan.nextInt();
+        }
+
+        // Until here
+
+        scan.close();
+
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
