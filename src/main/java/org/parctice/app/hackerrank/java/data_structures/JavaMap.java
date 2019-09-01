cd311260ab65be53a4ead0cbe4ddcbd15ff2a041
Alex <alex.d.bondarev@gmail.com>
2019-09-01 14:33:32 +0300

    add JavaMap

---
+++ src/main/java/org/parctice/app/hackerrank/java/data_structures/JavaMap.java
@@ -0,0 +1,32 @@
+package org.parctice.app.hackerrank.java.data_structures;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class JavaMap {
+    public static void main(String []argh){
+        Map<String, Integer> phoneBook = new HashMap<>();
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        in.nextLine();
+
+        for(int i=0;i<n;i++){
+            String name=in.nextLine();
+            int phone=in.nextInt();
+            in.nextLine();
+
+            phoneBook.put(name, phone);
+        }
+
+        while(in.hasNext()){
+            String s=in.nextLine();
+
+            if(phoneBook.containsKey(s)){
+                System.out.println(s+"="+phoneBook.get(s));
+            } else {
+                System.out.println("Not found");
+            }
+        }
+    }
+}
